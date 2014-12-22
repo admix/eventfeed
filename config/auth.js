effb942b22422d42ccfe0cf33c0f181ca4192bc6
Alexander Snurnikov <admix.snurnikov@gmail.com>
2014-12-22 11:08:01 -0500

    Loading from env file

--- config/auth.js
+++ config/auth.js
@@ -1,22 +1,23 @@
+var habitat = require("habitat");
+habitat.load();
+var env = new habitat();
 module.exports = {
-
 	'facebookAuth' : {
-		'clientID' 		:  '1469462683327868', // your App ID
-		'clientSecret' 	: '80f9d6c6109926cab1c642f406bd62c9', // your App Secret
-		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+		'clientID' 	:  env.get("FB_ID"),
+		'clientSecret' 	: env.get("FB_SECRET"),
+		'callbackURL' 	: env.get("FB_CALLBACK")
 	},
-	
+
 	'twitterAuth' : {
-		'consumerKey' 		: 'azatKhGtMHOIuJP6ixJJfffzw',
-		'consumerSecret' 	: 'I9qwU244CETUCvckBk8RYdk6vyMdqAHcm94kMkvUTxJIObatVJ',
-		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
-	},
+		'consumerKey' 	: env.get("TW_ID"),
+		'consumerSecret': env.get("TW_SECRET"),
+		'callbackURL' 	: env.get("TW_CALLBACK")
+	}
 	
 	'googleAuth' : {
-		'clientID' 		: '57060957916-57uijbmjopch07m6kp7akad5kuhnnhmi.apps.googleusercontent.com',
+		'clientID' 	: '57060957916-57uijbmjopch07m6kp7akad5kuhnnhmi.apps.googleusercontent.com',
 		'clientSecret' 	: 'N6oOioVixigHiqS_FqH9Br1f ',
 		'callbackURL' 	: 'http://127.0.0.1:8080/auth/google/callback'
 	}
 	
-	
-	};
+};
